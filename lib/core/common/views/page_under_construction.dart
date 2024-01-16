90e24ab3a072cda04d0f2da7e54e3ada51608c69
Sergey Kazberovich <kazbers@gmail.com>
2024-01-16 13:54:25 +0400

    implemented PageUnderConstruction via Lottie, and stub for Onboarding screen

---
+++ lib/core/common/views/page_under_construction.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:tdd_education_app/core/res/media_resources.dart';
+
+class PageUnderConstruction extends StatelessWidget {
+  const PageUnderConstruction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              MediaRes.onBoardingBackground,
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Lottie.asset(
+              MediaRes.pageUnderConstruction,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/onboarding/presentation/onboarding_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  static const routeName = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
