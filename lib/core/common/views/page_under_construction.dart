e4743f38e53953255253e94a25d4e8e00b67bf74
Sergey Kazberovich <kazbers@gmail.com>
2024-01-23 18:09:46 +0400

    finished with onboarding screen ui

--- lib/core/common/views/page_under_construction.dart
+++ lib/core/common/views/page_under_construction.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:tdd_education_app/core/common/widgets/gradient_background.dart';
 import 'package:tdd_education_app/core/res/media_resources.dart';
 
 class PageUnderConstruction extends StatelessWidget {
@@ -8,23 +9,9 @@ class PageUnderConstruction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        constraints: const BoxConstraints.expand(),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-              MediaRes.onBoardingBackground,
-            ),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: SafeArea(
-          child: Center(
-            child: Lottie.asset(
-              MediaRes.pageUnderConstruction,
-            ),
-          ),
-        ),
+      body: GradientBackground(
+        image: MediaRes.onBoardingBackground,
+        child: Lottie.asset(MediaRes.pageUnderConstruction),
       ),
     );
   }
