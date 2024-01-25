7f22738522ba19fbbdfe8e391096230af6117eb7
Sergey Kazberovich <kazbers@gmail.com>
2024-01-25 17:25:33 +0400

    added usecases for forgot password, sign in, sign up

---
+++ lib/src/authentication/domain/usecases/forgot_password.dart
@@ -0,0 +1,12 @@
+import 'package:tdd_education_app/core/usecases/usecases.dart';
+import 'package:tdd_education_app/core/utils/typedefs.dart';
+import 'package:tdd_education_app/src/authentication/domain/repositories/authentication_repository.dart';
+
+class ForgotPasswordUsecase extends UsecaseWithParams<void, String> {
+  const ForgotPasswordUsecase(this._repository);
+
+  final AuthenticationRepository _repository;
+
+  @override
+  ResultFuture<void> call(String params) => _repository.forgotPassword(params);
+}
---
+++ lib/src/authentication/domain/usecases/sign_in.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+import 'package:tdd_education_app/core/usecases/usecases.dart';
+import 'package:tdd_education_app/core/utils/typedefs.dart';
+import 'package:tdd_education_app/src/authentication/domain/entities/user.dart';
+import 'package:tdd_education_app/src/authentication/domain/repositories/authentication_repository.dart';
+
+class SignInUsecase extends UsecaseWithParams<LocalUser, SignInParams> {
+  SignInUsecase(this._repository);
+
+  final AuthenticationRepository _repository;
+
+  @override
+  ResultFuture<LocalUser> call(SignInParams params) =>
+      _repository.signIn(email: params.email, password: params.password);
+}
+
+class SignInParams extends Equatable {
+  const SignInParams({
+    required this.email,
+    required this.password,
+  });
+
+  const SignInParams.empty()
+      : email = '',
+        password = '';
+
+  final String email;
+  final String password;
+
+  @override
+  List<Object?> get props => [email, password];
+}
