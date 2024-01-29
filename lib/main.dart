353bff3fb982de7805b2fc730e080c3311ca6495
Sergey Kazberovich <kazbers@gmail.com>
2024-01-29 23:36:16 +0400

    set up firebase core, auth, storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tdd_education_app/core/res/colours.dart';
 import 'package:tdd_education_app/core/res/fonts.dart';
 import 'package:tdd_education_app/core/services/injection_container.dart';
 import 'package:tdd_education_app/core/services/router.dart';
-import 'package:tdd_education_app/src/onboarding/presentation/views/onboarding_screen.dart';
+
+import 'package:tdd_education_app/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await init();
   runApp(const MyApp());
 }
--- lib/src/authentication/data/datasources/authentication_remote_datasource.dart
+++ lib/src/authentication/data/datasources/authentication_remote_datasource.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:tdd_education_app/core/enums/update_user.dart';
 import 'package:tdd_education_app/src/authentication/data/models/user_model.dart';
 
@@ -21,4 +24,43 @@ abstract class AuthenticationRemoteDatasource {
     required UpdateUserAction action,
     required dynamic userData,
   });
+}
+
+class AuthenticationRemoteDataSourceImplementation
+    implements AuthenticationRemoteDatasource {
+  const AuthenticationRemoteDataSourceImplementation(
+      this._authClient, this._cloudStoreClient, this._dbClient);
+
+  final FirebaseAuth _authClient;
+  final FirebaseFirestore _cloudStoreClient;
+  final FirebaseStorage _dbClient;
+
+  @override
+  Future<void> forgotPassword(String email) async {
+    // TODO: implement forgotPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<LocalUserModel> signIn(
+      {required String email, required String password}) async {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp(
+      {required String email,
+      required String fullName,
+      required String password}) async {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateUser(
+      {required UpdateUserAction action, required dynamic userData}) async {
+    // TODO: implement updateUser
+    throw UnimplementedError();
+  }
 }
