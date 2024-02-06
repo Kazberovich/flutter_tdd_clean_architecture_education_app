d19893940df2348c6766f3564271e4c7e4a59afb
Sergey Kazberovich <kazbers@gmail.com>
2024-02-06 21:07:28 +0400

    added DI for Authentication module

--- lib/core/services/injection_container.dart
+++ lib/core/services/injection_container.dart
@@ -1,5 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tdd_education_app/src/authentication/data/datasources/authentication_remote_datasource.dart';
+import 'package:tdd_education_app/src/authentication/data/repositories/authentication_repository_implementation.dart';
+import 'package:tdd_education_app/src/authentication/domain/repositories/authentication_repository.dart';
+import 'package:tdd_education_app/src/authentication/domain/usecases/forgot_password.dart';
+import 'package:tdd_education_app/src/authentication/domain/usecases/sign_in.dart';
+import 'package:tdd_education_app/src/authentication/domain/usecases/sign_up.dart';
+import 'package:tdd_education_app/src/authentication/domain/usecases/update_user.dart';
+import 'package:tdd_education_app/src/authentication/presentation/bloc/authentication_bloc.dart';
 import 'package:tdd_education_app/src/onboarding/data/datasources/onboarding_local_datasource.dart';
 import 'package:tdd_education_app/src/onboarding/data/repositories/onboarding_repository_implementation.dart';
 import 'package:tdd_education_app/src/onboarding/domain/repositories/onboarding_repository.dart';
@@ -10,6 +21,45 @@ import 'package:tdd_education_app/src/onboarding/presentation/cubit/onboarding_c
 final serviceLocator = GetIt.instance;
 
 Future<void> init() async {
+  await _initOnboarding();
+  await _initAuth();
+}
+
+Future<void> _initAuth() async {
+  serviceLocator
+    ..registerFactory(() => AuthenticationBloc(
+        signInUsecase: serviceLocator(),
+        signUpUsecase: serviceLocator(),
+        forgotPasswordUsecase: serviceLocator(),
+        updateUserUsecase: serviceLocator()))
+    ..registerLazySingleton(
+      () => SignInUsecase(serviceLocator()),
+    )
+    ..registerLazySingleton(
+      () => SignUpUsecase(serviceLocator()),
+    )
+    ..registerLazySingleton(
+      () => UpdateUserUsecase(serviceLocator()),
+    )
+    ..registerLazySingleton(
+      () => ForgotPasswordUsecase(serviceLocator()),
+    )
+    ..registerLazySingleton<AuthenticationRepository>(
+      () => AuthenticationRepositoryImplementation(serviceLocator()),
+    )
+    ..registerLazySingleton<AuthenticationRemoteDatasource>(
+      () => AuthenticationRemoteDataSourceImplementation(
+        serviceLocator(),
+        serviceLocator(),
+        serviceLocator(),
+      ),
+    )
+    ..registerLazySingleton(() => FirebaseAuth.instance)
+    ..registerLazySingleton(() => FirebaseFirestore.instance)
+    ..registerLazySingleton(() => FirebaseStorage.instance);
+}
+
+Future<void> _initOnboarding() async {
   final prefs = await SharedPreferences.getInstance();
   // Feature -> Onboarding Feature
   // Business Logic
