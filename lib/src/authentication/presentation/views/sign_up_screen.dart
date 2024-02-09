3572e1bff7d8190fb6be89e7ac15bacbce19a8cb
Sergey Kazberovich <kazbers@gmail.com>
2024-02-09 22:46:19 +0400

    implemented [SignUpScreen]

--- lib/src/authentication/presentation/views/sign_up_screen.dart
+++ lib/src/authentication/presentation/views/sign_up_screen.dart
@@ -1,5 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:tdd_education_app/core/extensions/context_extension.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tdd_education_app/core/common/app/providers/user_provider.dart';
+import 'package:tdd_education_app/core/common/widgets/gradient_background.dart';
+import 'package:tdd_education_app/core/common/widgets/rounded_button.dart';
+import 'package:tdd_education_app/core/res/fonts.dart';
+import 'package:tdd_education_app/core/res/media_resources.dart';
+import 'package:tdd_education_app/core/utils/core_utils.dart';
+import 'package:tdd_education_app/src/authentication/data/models/user_model.dart';
+import 'package:tdd_education_app/src/authentication/presentation/bloc/authentication_bloc.dart';
+import 'package:tdd_education_app/src/authentication/presentation/views/sign_in_screen.dart';
+import 'package:tdd_education_app/src/authentication/presentation/widgets/sign_in_form.dart';
+import 'package:tdd_education_app/src/authentication/presentation/widgets/sign_up_form.dart';
+import 'package:tdd_education_app/src/dashboard/presentation/views/dashboard_screen.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -11,16 +24,120 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController fullNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    fullNameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      child: Center(
-        child: Text(
-          'SIGN UP SCREEN',
-          style: context.theme.textTheme.bodyLarge?.copyWith(
-            color: Colors.white,
-          ),
-        ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: BlocConsumer<AuthenticationBloc, AuthenticationState>(
+        listener: (context, state) {
+          if (state is AuthenticationError) {
+            CoreUtils.showSnackBar(context, state.message);
+          } else if (state is SignedUp) {
+            context.read<AuthenticationBloc>().add(
+                  SignInEvent(
+                    email: emailController.text.trim(),
+                    password: passwordController.text.trim(),
+                  ),
+                );
+          } else if (state is SignedIn) {
+            context.read<UserProvider>().initUser(state.user as LocalUserModel);
+            Navigator.pushReplacementNamed(context, DashboardScreen.routeName);
+          }
+        },
+        builder: (context, state) {
+          return GradientBackground(
+            image: MediaRes.authGradientBackground,
+            child: SafeArea(
+              child: Center(
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  children: [
+                    const Text(
+                      'Easy to learn, discover more skills',
+                      style: TextStyle(
+                        fontFamily: Fonts.aeonik,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 32,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Sign up for an account',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                            context,
+                            SignInScreen.routeName,
+                          );
+                        },
+                        child: const Text('Already have an account?'),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SignUpForm(
+                      emailController: emailController,
+                      passwordController: passwordController,
+                      fullNameController: fullNameController,
+                      confirmPasswordController: confirmPasswordController,
+                      formKey: formKey,
+                    ),
+                    const SizedBox(height: 30),
+                    if (state is AuthenticationLoading)
+                      const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    else
+                      RoundedButton(
+                        label: 'Sign Up',
+                        onPressed: () {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          FirebaseAuth.instance.currentUser?.reload();
+                          if (formKey.currentState!.validate()) {
+                            context.read<AuthenticationBloc>().add(
+                                  SignUpEvent(
+                                    email: emailController.text.trim(),
+                                    password: passwordController.text.trim(),
+                                    name: fullNameController.text.trim(),
+                                  ),
+                                );
+                          }
+                        },
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
