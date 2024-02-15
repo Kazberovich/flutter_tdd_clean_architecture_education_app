9845be200c04984aeace64bacfc25f727b41ab21
Sergey Kazberovich <kazbers@gmail.com>
2024-02-15 23:55:05 +0400

    added [ProfileBody] and finished with [ProfileView]

---
+++ lib/src/profile/presentation/refactors/profile_body.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+import 'package:provider/provider.dart';
+import 'package:tdd_education_app/core/common/app/providers/user_provider.dart';
+import 'package:tdd_education_app/core/res/colours.dart';
+import 'package:tdd_education_app/core/res/media_resources.dart';
+
+import 'package:tdd_education_app/src/profile/presentation/widgets/user_info_card.dart';
+
+class ProfileBody extends StatelessWidget {
+  const ProfileBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<UserProvider>(builder: (_, provider, __) {
+      final user = provider.user;
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: UserInfoCard(
+                  infoThemeColor: Colours.physicsTileColour,
+                  infoIcon: const Icon(
+                    IconlyLight.document,
+                    size: 24,
+                    color: Color(0xFF767DFF),
+                  ),
+                  infoTitle: 'Courses',
+                  infoValue: user!.enrolledCourseIds.length.toString(),
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: UserInfoCard(
+                  infoThemeColor: Colours.languageTileColour,
+                  infoIcon: Image.asset(
+                    MediaRes.scoreboard,
+                    height: 24,
+                    width: 24,
+                  ),
+                  infoTitle: 'Score',
+                  infoValue: user.points.toString(),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                child: UserInfoCard(
+                  infoThemeColor: Colours.biologyTileColour,
+                  infoIcon: const Icon(
+                    IconlyLight.user,
+                    color: Color(0xFF56AEFF),
+                    size: 24,
+                  ),
+                  infoTitle: 'Followers',
+                  infoValue: user.followers.length.toString(),
+                ),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: UserInfoCard(
+                  infoThemeColor: Colours.chemistryTileColour,
+                  infoIcon: const Icon(
+                    IconlyLight.user,
+                    color: Color(
+                      0xFFFF84AA,
+                    ),
+                    size: 24,
+                  ),
+                  infoTitle: 'Following',
+                  infoValue: user.following.length.toString(),
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
+    });
+  }
+}
--- lib/src/profile/presentation/views/profile_view.dart
+++ lib/src/profile/presentation/views/profile_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tdd_education_app/core/common/widgets/gradient_background.dart';
 import 'package:tdd_education_app/core/res/media_resources.dart';
+import 'package:tdd_education_app/src/profile/presentation/refactors/profile_body.dart';
 import 'package:tdd_education_app/src/profile/presentation/refactors/profile_header.dart';
 import 'package:tdd_education_app/src/profile/presentation/widgets/profile_app_bar.dart';
 
@@ -17,7 +18,10 @@ class ProfileView extends StatelessWidget {
         image: MediaRes.profileGradientBackground,
         child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          children: const [ProfileHeader()],
+          children: const [
+            ProfileHeader(),
+            ProfileBody(),
+          ],
         ),
       ),
     );
