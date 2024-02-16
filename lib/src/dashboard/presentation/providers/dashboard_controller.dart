de44dcc632f1f11db0b0f218e0042414dc085526
Sergey Kazberovich <kazbers@gmail.com>
2024-02-16 15:40:57 +0400

    injected [AuthenticationBloc] into the widget tree

--- lib/src/dashboard/presentation/providers/dashboard_controller.dart
+++ lib/src/dashboard/presentation/providers/dashboard_controller.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:tdd_education_app/core/common/app/providers/tab_navigator.dart';
 import 'package:tdd_education_app/core/common/views/persistent_view.dart';
+import 'package:tdd_education_app/core/services/injection_container.dart';
+import 'package:tdd_education_app/src/authentication/presentation/bloc/authentication_bloc.dart';
 import 'package:tdd_education_app/src/profile/presentation/views/profile_view.dart';
 
 class DashboardController extends ChangeNotifier {
@@ -35,7 +38,10 @@ class DashboardController extends ChangeNotifier {
     ChangeNotifierProvider(
       create: (_) => TabNavigator(
         TabItem(
-          child: const ProfileView(),
+          child: BlocProvider(
+            create: (_) => serviceLocator<AuthenticationBloc>(),
+            child: const ProfileView(),
+          ),
         ),
       ),
       child: const PersistentView(),
