943c25e7bf3da7d467824b9a3e3a73db87696790
Sergey Kazberovich <kazbers@gmail.com>
2024-02-23 16:45:45 +0400

    added empty constructor to [Course]

--- lib/core/common/features/course/domain/entities/course.dart
+++ lib/core/common/features/course/domain/entities/course.dart
@@ -15,6 +15,19 @@ class Course extends Equatable {
     this.imageIsFile = false,
   });
 
+  Course.empty()
+      : this(
+          id: '_empty.id',
+          title: '_empty.title',
+          description: '_empty.description',
+          numberOfExams: 0,
+          numberOfMaterials: 0,
+          numberOfVideos: 0,
+          groupId: '_empty.groupId',
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now(),
+        );
+
   final String id;
   final String title;
   final String? description;
