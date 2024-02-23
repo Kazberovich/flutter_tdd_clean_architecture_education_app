0e8d6acce43d0db47fc1033317c79eb192843be4
Sergey Kazberovich <kazbers@gmail.com>
2024-02-23 16:22:34 +0400

    added [Course] entity under domain layer

---
+++ lib/core/common/features/course/domain/entities/course.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+
+class Course extends Equatable {
+  final String id;
+  final String title;
+  final String? description;
+  final int numberOfExams;
+  final int numberOfMaterials;
+  final int numberOfVideos;
+  final String groupId;
+  final String? image;
+  final bool imageIsFile;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  const Course({
+    required this.id,
+    required this.title,
+    required this.numberOfExams,
+    required this.numberOfMaterials,
+    required this.numberOfVideos,
+    required this.groupId,
+    required this.createdAt,
+    required this.updatedAt,
+    this.description,
+    this.image,
+    this.imageIsFile = false,
+  });
+
+  @override
+  List<Object?> get props => [id];
+}
