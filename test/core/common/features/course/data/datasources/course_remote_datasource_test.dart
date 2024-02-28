1fb6297aab32ccb27a45f2427e4f5976973106d4
Sergey Kazberovich <kazbers@gmail.com>
2024-02-28 13:26:22 +0400

    added test for addCourse if successfull

--- test/core/common/features/course/data/datasources/course_remote_datasource_test.dart
+++ test/core/common/features/course/data/datasources/course_remote_datasource_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
@@ -5,6 +7,7 @@ import 'package:firebase_storage_mocks/firebase_storage_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_sign_in_mocks/google_sign_in_mocks.dart';
 import 'package:tdd_education_app/core/common/features/course/data/datasources/course_remote_datasource.dart';
+import 'package:tdd_education_app/core/common/features/course/data/models/course_model.dart';
 
 void main() {
   late CourseRemoteDataSource remoteDataSource;
@@ -35,5 +38,29 @@ void main() {
       storage: storage,
       auth: auth,
     );
+  });
+
+  group('addCourse', () {
+    test('should add the given course to the firestore collection', () async {
+      // Arrange
+      final course = CourseModel.empty();
+      // Act
+      await remoteDataSource.addCourse(course);
+      // Assert
+      final firestoreData = await firestore.collection('courses').get();
+      expect(firestoreData.docs.length, 1);
+
+      final courseRef = firestoreData.docs.first;
+      expect(courseRef.data()['id'], courseRef.id);
+
+      final groupData = await firestore.collection('groups').get();
+      expect(groupData.docs.length, 1);
+
+      final groupRef = groupData.docs.first;
+      expect(groupRef.data()['id'], groupRef.id);
+
+      expect(courseRef.data()['groupId'], groupRef.id);
+      expect(courseRef.data()['courseId'], courseRef.id);
+    });
   });
 }
