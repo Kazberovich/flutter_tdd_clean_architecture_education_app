195b093b50f88f8d4c72cc474559eb3b895eb31c
Sergey Kazberovich <kazbers@gmail.com>
2024-03-04 14:18:59 +0400

    added course of the day notifyer

---
+++ lib/core/common/app/providers/course_of_the_day_notifier.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:tdd_education_app/src/course/domain/entities/course.dart';
+
+class CourseOfTheDayNotifier extends ChangeNotifier {
+  Course? _courseOfTheDay;
+
+  Course? get courseOfTheDay => _courseOfTheDay;
+
+  void setCourseOfTheDay(Course course) {
+    _courseOfTheDay ??= course;
+    notifyListeners();
+  }
+}
