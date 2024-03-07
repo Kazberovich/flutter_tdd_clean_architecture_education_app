57ead188e22cced3aa3ade4ef6532f32c1b11af1
Sergey Kazberovich <kazbers@gmail.com>
2024-03-07 23:42:22 +0400

    implemented expandable text widget [ExpandableText]

--- lib/src/course/presentation/views/course_details_screen.dart
+++ lib/src/course/presentation/views/course_details_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tdd_education_app/core/common/widgets/expandable_text.dart';
+import 'package:tdd_education_app/core/common/widgets/gradient_background.dart';
+import 'package:tdd_education_app/core/extensions/context_extension.dart';
+import 'package:tdd_education_app/core/res/media_resources.dart';
 import 'package:tdd_education_app/src/course/domain/entities/course.dart';
 
 class CourseDetailsScreen extends StatelessWidget {
@@ -10,6 +14,48 @@ class CourseDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text(course.title),
+      ),
+      body: GradientBackground(
+        image: MediaRes.homeGradientBackground,
+        child: SafeArea(
+          child: ListView(
+            padding: const EdgeInsets.all(20),
+            children: [
+              SizedBox(
+                height: context.height * .3,
+                child: Center(
+                  child: course.image != null
+                      ? Image.network(course.image!)
+                      : Image.asset(
+                          MediaRes.casualMeditation,
+                        ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    course.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 24,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  if(course.description != null)
+                    ExpandableText(text: course.description!, context: context),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
