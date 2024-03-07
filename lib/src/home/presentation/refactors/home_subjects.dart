c6b0d6aa2e6fee710ee5218bfa4358e15655316b
Sergey Kazberovich <kazbers@gmail.com>
2024-03-07 22:29:32 +0400

    added [CourseTile]

--- lib/src/home/presentation/refactors/home_subjects.dart
+++ lib/src/home/presentation/refactors/home_subjects.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tdd_education_app/core/common/widgets/course_tile.dart';
 import 'package:tdd_education_app/core/extensions/context_extension.dart';
+import 'package:tdd_education_app/core/res/colours.dart';
 
 import 'package:tdd_education_app/src/course/domain/entities/course.dart';
 import 'package:tdd_education_app/src/home/presentation/views/section_header.dart';
@@ -24,6 +26,33 @@ class HomeSubjects extends StatelessWidget {
           onSeeAll: () => context.push(
             const Placeholder(),
           ),
+        ),
+        const Text(
+          'Explore our courses',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            color: Colours.neutralTextColour,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: courses
+              .take(4)
+              .map(
+                (course) => CourseTile(
+                  course: course,
+                  // TODO(Course-Details): change to pushNamed and route
+                  onTap: () => Navigator.of(context).pushNamed(
+                    '/unknown',
+                    arguments: course,
+                  ),
+                ),
+              )
+              .toList(),
         ),
       ],
     );
