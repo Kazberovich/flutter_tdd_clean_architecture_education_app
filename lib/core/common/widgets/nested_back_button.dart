17888bd66ef1fcaac777e6e0a788554bb211e043
Sergey Kazberovich <kazbers@gmail.com>
2024-03-08 23:24:47 +0400

    cleaned up the code

--- lib/core/common/widgets/nested_back_button.dart
+++ lib/core/common/widgets/nested_back_button.dart
@@ -22,9 +22,7 @@ class NestedBackButton extends StatelessWidget {
             Navigator.of(context).pop();
           }
         },
-        icon: Theme
-            .of(context)
-            .platform == TargetPlatform.iOS
+        icon: Theme.of(context).platform == TargetPlatform.iOS
             ? const Icon(Icons.arrow_back_ios_new)
             : const Icon(Icons.arrow_back),
       ),
