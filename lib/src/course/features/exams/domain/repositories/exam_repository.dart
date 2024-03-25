c87e015b4fa332698aa774d3ea0e2bd80c8c3bc7
Sergey Kazberovich <kazbers@gmail.com>
2024-03-25 17:15:09 +0400

    added repository [ExamRepository] with listed methods

---
+++ lib/src/course/features/exams/domain/repositories/exam_repository.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tdd_education_app/core/utils/typedefs.dart';
+import 'package:tdd_education_app/src/course/features/exams/domain/entities/exam.dart';
+import 'package:tdd_education_app/src/course/features/exams/domain/entities/exam_question.dart';
+import 'package:tdd_education_app/src/course/features/exams/domain/entities/user_exam.dart';
+
+abstract class ExamRepository {
+  ResultFuture<List<Exam>> getExams(String courseId);
+
+  ResultFuture<List<ExamQuestion>> getExamQuestions(Exam exam);
+
+  ResultFuture<void> uploadExam(Exam exam);
+
+  ResultFuture<void> update(Exam exam);
+
+  ResultFuture<void> submitExam(UserExam userExam);
+
+  ResultFuture<List<UserExam>> getUserExams();
+
+  ResultFuture<List<UserExam>> getUserCourseExams(String courseId);
+}
