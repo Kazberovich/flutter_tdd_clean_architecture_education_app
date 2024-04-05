a53b9c03328dfcd48d2ee030567023a3edf5744b
Sergey Kazberovich <kazbers@gmail.com>
2024-04-05 15:47:43 +0400

    added notification model in data module

---
+++ lib/src/notifications/data/models/notification_model.dart
@@ -0,0 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tdd_education_app/core/enums/notification_enum.dart';
+import 'package:tdd_education_app/core/extensions/enum_extensions.dart';
+import 'package:tdd_education_app/core/utils/typedefs.dart';
+import 'package:tdd_education_app/src/notifications/domain/entities/notification.dart';
+
+class NotificationModel extends Notification {
+  const NotificationModel({
+    required super.id,
+    required super.title,
+    required super.body,
+    required super.category,
+    required super.sentAt,
+    required super.seen,
+  });
+
+  NotificationModel.fromMap(DataMap map)
+      : super(
+          id: map['id'] as String,
+          title: map['title'] as String,
+          body: map['body'] as String,
+          category: (map['category'] as String).toNotificationCategory,
+          seen: map['seen'] as bool,
+          sentAt: (map['sentAt'] as Timestamp).toDate(),
+        );
+
+  NotificationModel.empty()
+      : this(
+          id: '_empty.id',
+          title: '_empty.title',
+          body: '_empty.body',
+          category: NotificationCategory.NONE,
+          seen: false,
+          sentAt: DateTime.now(),
+        );
+
+  NotificationModel copyWith({
+    String? id,
+    String? title,
+    String? body,
+    NotificationCategory? category,
+    bool? seen,
+    DateTime? sentAt,
+  }) {
+    return NotificationModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      body: body ?? this.body,
+      category: category ?? this.category,
+      seen: seen ?? this.seen,
+      sentAt: sentAt ?? this.sentAt,
+    );
+  }
+
+  DataMap toMap() => {
+        'id': id,
+        'title': title,
+        'body': body,
+        'category': category.value,
+        'seen': seen,
+        'sentAt': FieldValue.serverTimestamp(),
+      };
+}
