35d031f0f40128ac81d8d2a0e38b5e0325112f06
Sergey Kazberovich <kazbers@gmail.com>
2024-04-08 13:09:11 +0400

    prepared remote datasource implementation and testing for implementation

--- lib/src/notifications/data/datasources/notification_remote_datasource.dart
+++ lib/src/notifications/data/datasources/notification_remote_datasource.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:tdd_education_app/src/notifications/data/models/notification_model.dart';
 import 'package:tdd_education_app/src/notifications/domain/entities/notification.dart';
 
@@ -13,4 +15,45 @@ abstract class NotificationRemoteDatasource {
   Future<void> sendNotification(Notification notification);
 
   Stream<List<NotificationModel>> getNotifications();
+}
+
+class NotificationRemoteDatasourceImplementation
+    implements NotificationRemoteDatasource {
+  NotificationRemoteDatasourceImplementation(
+      {required FirebaseFirestore firestore, required FirebaseAuth auth})
+      : _firestore = firestore,
+        _auth = auth;
+
+  final FirebaseFirestore _firestore;
+  final FirebaseAuth _auth;
+
+  @override
+  Future<void> clear(String notificationId) {
+    // TODO: implement clear
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> clearAll() {
+    // TODO: implement clearAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<List<NotificationModel>> getNotifications() {
+    // TODO: implement getNotifications
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> markAsRead(String notificationId) {
+    // TODO: implement markAsRead
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendNotification(Notification notification) {
+    // TODO: implement sendNotification
+    throw UnimplementedError();
+  }
 }
---
+++ test/src/notifications/data/datasources/notification_remote_datasource_implementation_test.dart
@@ -0,0 +1,38 @@
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:firebase_storage_mocks/firebase_storage_mocks.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_sign_in_mocks/google_sign_in_mocks.dart';
+import 'package:tdd_education_app/src/notifications/data/datasources/notification_remote_datasource.dart';
+
+void main() {
+  late NotificationRemoteDatasourceImplementation notificationRemoteDatasource;
+  late FakeFirebaseFirestore firestore;
+  late MockFirebaseAuth auth;
+  late MockFirebaseStorage storage;
+
+  setUp(() async {
+    firestore = FakeFirebaseFirestore();
+
+    final user =
+        MockUser(uid: 'uid', email: 'email', displayName: 'displayName');
+
+    final googleSignIn = MockGoogleSignIn();
+    final signInAccount = await googleSignIn.signIn();
+    final googleAuth = await signInAccount!.authentication;
+    final credentials = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    auth = MockFirebaseAuth(mockUser: user);
+    await auth.signInWithCredential(credentials);
+
+    storage = MockFirebaseStorage();
+
+    notificationRemoteDatasource = NotificationRemoteDatasourceImplementation(
+      firestore: firestore,
+      auth: auth,
+    );
+  });
+}
