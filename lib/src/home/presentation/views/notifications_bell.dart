612d4f8ee3fd27cfe97fbdb1be7f36e7de9d1eee
Sergey Kazberovich <kazbers@gmail.com>
2024-04-09 22:29:57 +0400

    implemented notification bell ui with help of Badge library

---
+++ lib/src/home/presentation/views/notifications_bell.dart
@@ -0,0 +1,45 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart' hide Badge;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:iconly/iconly.dart';
+import 'package:tdd_education_app/src/notifications/presentation/cubit/notification_cubit.dart';
+
+class NotificationBell extends StatefulWidget {
+  const NotificationBell({super.key});
+
+  @override
+  State<NotificationBell> createState() => _NotificationBellState();
+}
+
+class _NotificationBellState extends State<NotificationBell> {
+  @override
+  void initState() {
+    super.initState();
+
+    context.read<NotificationCubit>().getNotifications();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<NotificationCubit, NotificationState>(
+      builder: (context, state) {
+        if (state is NotificationsLoaded) {
+          final unseenNotificationsLength = state.notifications
+              .where((notification) => !notification.seen)
+              .length;
+          final showBadge = unseenNotificationsLength > 0;
+          return Badge(
+            showBadge: showBadge,
+            position: BadgePosition.topEnd(top: -16, end: -11),
+            badgeContent: Text(
+              unseenNotificationsLength.toString(),
+              style: const TextStyle(color: Colors.white, fontSize: 12),
+            ),
+            child: const Icon(IconlyLight.notification),
+          );
+        }
+        return const Icon(IconlyLight.notification);
+      },
+    );
+  }
+}
