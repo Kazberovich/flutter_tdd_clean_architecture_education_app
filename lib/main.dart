31c377e406d6f3fca0bd981804b254dc5d52e6a9
Sergey Kazberovich <kazbers@gmail.com>
2024-04-11 23:14:15 +0400

    inserted NotificationsNotifier provider into the widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart' as uiauth;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tdd_education_app/core/common/app/providers/course_of_the_day_notifier.dart';
+import 'package:tdd_education_app/core/common/app/providers/notifications_notifier.dart';
 import 'package:tdd_education_app/core/common/app/providers/user_provider.dart';
 import 'package:tdd_education_app/core/res/colours.dart';
 import 'package:tdd_education_app/core/res/fonts.dart';
@@ -35,6 +37,10 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => UserProvider()),
         ChangeNotifierProvider(create: (_) => DashboardController()),
         ChangeNotifierProvider(create: (_) => CourseOfTheDayNotifier()),
+        ChangeNotifierProvider(
+          create: (_) =>
+              NotificationsNotifier(serviceLocator<SharedPreferences>()),
+        ),
       ],
       child: MaterialApp(
         title: 'Flutter TDD Education App',
