2302d4376dad87b14cc1a9c62e9dc290a9ed1628
Sergey Kazberovich <kazbers@gmail.com>
2024-04-12 15:31:35 +0400

    added int and datetime extension for notification ''seen X time ago' ui

---
+++ lib/core/extensions/int_extension.dart
@@ -0,0 +1,13 @@
+
+extension IntExt on int {
+  String get estimate {
+    if(this <= 10) return '$this';
+    var data = this - (this % 10);
+    if(data == this) data = this - 5;
+    return 'over $data';
+  }
+
+  String get pluralize {
+    return this > 1 ? 's' : '';
+  }
+}
