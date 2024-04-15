b2ceb9cec25ad8147d97aa30a003d5bcf58e106c
Sergey Kazberovich <kazbers@gmail.com>
2024-04-15 21:12:00 +0400

    started ui implementations adding exams, materials, and videos

---
+++ lib/src/course/features/videos/presentation/views/add_videos_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddVideosView extends StatefulWidget {
+  const AddVideosView({super.key});
+
+  static const routeName = '/add-videos';
+
+  @override
+  State<AddVideosView> createState() => _AddVideosViewState();
+}
+
+class _AddVideosViewState extends State<AddVideosView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
