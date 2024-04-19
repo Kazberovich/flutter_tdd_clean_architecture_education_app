4cba4a75a968eb466789fda268e51636232fa85d
Sergey Kazberovich <kazbers@gmail.com>
2024-04-19 17:27:35 +0400

    updated notification wrapper with showing dialog, also converted to stateful widget

--- lib/src/notifications/presentation/widgets/notification_wrapper.dart
+++ lib/src/notifications/presentation/widgets/notification_wrapper.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tdd_education_app/core/utils/core_utils.dart';
 
 import 'package:tdd_education_app/src/notifications/presentation/cubit/notification_cubit.dart';
 
-class NotificationWrapper extends StatelessWidget {
+class NotificationWrapper extends StatefulWidget {
   const NotificationWrapper({
     required this.child,
     required this.onNotificationSent,
@@ -16,15 +17,29 @@ class NotificationWrapper extends StatelessWidget {
   final VoidCallback onNotificationSent;
 
   @override
+  State<NotificationWrapper> createState() => _NotificationWrapperState();
+}
+
+class _NotificationWrapperState extends State<NotificationWrapper> {
+  bool showLoadingDialog = false;
+
+  @override
   Widget build(BuildContext context) {
     return BlocListener<NotificationCubit, NotificationState>(
       listener: (context, state) {
+        if(showLoadingDialog){
+          Navigator.pop(context);
+          showLoadingDialog = false;
+        }
         if (state is NotificationSent) {
-          extraActivity?.call();
-          onNotificationSent();
+          widget.extraActivity?.call();
+          widget.onNotificationSent();
+        } else if (state is SendingNotification) {
+          showLoadingDialog = true;
+          CoreUtils.showLoadingDialog(context);
         }
       },
-      child: child,
+      child: widget.child,
     );
   }
 }
