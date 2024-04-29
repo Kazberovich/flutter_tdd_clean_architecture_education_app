548e3a45d13912d35b8a0311e6f5ed78952255d1
Sergey Kazberovich <kazbers@gmail.com>
2024-04-29 16:56:49 +0400

    added skeleton for materials screen

--- lib/src/course/features/materials/presentation/views/course_materials_view.dart
+++ lib/src/course/features/materials/presentation/views/course_materials_view.dart
@@ -1,5 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide MaterialState;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tdd_education_app/core/common/views/loading_view.dart';
+import 'package:tdd_education_app/core/common/widgets/gradient_background.dart';
+import 'package:tdd_education_app/core/common/widgets/nested_back_button.dart';
+import 'package:tdd_education_app/core/common/widgets/not_found_text.dart';
+import 'package:tdd_education_app/core/res/media_resources.dart';
+import 'package:tdd_education_app/core/utils/core_utils.dart';
 import 'package:tdd_education_app/src/course/domain/entities/course.dart';
 import 'package:tdd_education_app/src/course/features/materials/presentation/cubit/material_cubit.dart';
 
@@ -32,6 +38,48 @@ class _CourseMaterialsViewState extends State<CourseMaterialsView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text('${widget.course.title} Materials'),
+        leading: const NestedBackButton(),
+      ),
+      body: GradientBackground(
+        image: MediaRes.documentsGradientBackground,
+        child: BlocConsumer<MaterialCubit, MaterialState>(
+          listener: (_, state) {
+            if (state is MaterialError) {
+              CoreUtils.showSnackBar(context, state.message);
+            }
+          },
+          builder: (_, state) {
+            if (state is LoadingMaterials) {
+              return const LoadingView();
+            } else if ((state is MaterialsLoaded && state.materials.isEmpty) ||
+                state is MaterialError) {
+              return NotFoundText(
+                'No materials found for ${widget.course.title}',
+              );
+            } else if (state is MaterialsLoaded) {
+              final materials = state.materials
+                ..sort((a, b) => b.uploadDate.compareTo(a.uploadDate));
+
+              return SafeArea(
+                child: ListView.separated(
+                  separatorBuilder: (_, __) => const Divider(
+                    color: Color(0xFFE6E8EC),
+                  ),
+                  padding: const EdgeInsets.all(20),
+                  itemBuilder: (_, index) {
+                    return Placeholder(fallbackHeight: 50,);
+                  },
+                  itemCount: materials.length,
+                ),
+              );
+            }
+            return const SizedBox.shrink();
+          },
+        ),
+      ),
     );
   }
 }
