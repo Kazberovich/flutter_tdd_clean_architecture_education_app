94d09a023352c6d54986dec6728eca3904f10d77
Sergey Kazberovich <kazbers@gmail.com>
2024-05-01 23:10:44 +0400

    added app bar and notification wrapper

--- lib/src/course/features/exams/presentation/views/add_exam_view.dart
+++ lib/src/course/features/exams/presentation/views/add_exam_view.dart
@@ -2,14 +2,16 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tdd_education_app/core/common/widgets/course_picker.dart';
+import 'package:tdd_education_app/core/enums/notification_enum.dart';
 import 'package:tdd_education_app/core/utils/core_utils.dart';
 import 'package:tdd_education_app/core/utils/typedefs.dart';
 import 'package:tdd_education_app/src/course/domain/entities/course.dart';
 import 'package:tdd_education_app/src/course/features/exams/data/models/exam_model.dart';
 import 'package:tdd_education_app/src/course/features/exams/presentation/cubit/exam_cubit.dart';
+import 'package:tdd_education_app/src/notifications/presentation/widgets/notification_wrapper.dart';
 
 class AddExamView extends StatefulWidget {
   const AddExamView({super.key});
@@ -52,6 +54,8 @@ class _AddExamViewState extends State<AddExamView> {
     }
   }
 
+  bool showingDialog = false;
+
   @override
   void dispose() {
     courseNotifier.dispose();
@@ -61,6 +65,54 @@ class _AddExamViewState extends State<AddExamView> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return NotificationWrapper(
+      child: BlocListener<ExamCubit, ExamState>(
+        listener: (context, state) {
+          if (showingDialog == true) {
+            Navigator.pop(context);
+            showingDialog = false;
+          }
+          if (state is UploadingExam) {
+            CoreUtils.showLoadingDialog(context);
+            showingDialog = true;
+          } else if (state is ExamError) {
+            CoreUtils.showSnackBar(context, state.message);
+          } else if (state is ExamUploaded) {
+            CoreUtils.showSnackBar(context, 'Exam added successfully');
+            CoreUtils.sendNotification(
+              context,
+              title: 'New (${courseNotifier.value!.title.trim()} exam)',
+              body: 'A new exam has been added',
+              category: NotificationCategory.TEST,
+            );
+          }
+        },
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(title: const Text('Add Exam')),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: Column(
+                  children: [
+                    Form(
+                      key: formKey,
+                      child: CoursePicker(
+                        controller: courseController,
+                        notifier: courseNotifier,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      onNotificationSent: () {
+        Navigator.pop(context);
+      },
+    );
   }
 }
