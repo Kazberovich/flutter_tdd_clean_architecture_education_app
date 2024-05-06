eedd366e8f38bf7188b2d474502862c8ac68e4ac
Sergey Kazberovich <kazbers@gmail.com>
2024-05-06 23:08:00 +0400

    added a skeleton class for exam view

---
+++ lib/src/course/features/exams/presentation/views/exam_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExamView extends StatefulWidget {
+  const ExamView({super.key});
+
+  @override
+  State<ExamView> createState() => _ExamViewState();
+}
+
+class _ExamViewState extends State<ExamView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
