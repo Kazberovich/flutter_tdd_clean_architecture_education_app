cf6b7b58eec52d4c7c8e8e0a989aec99f50d29f1
Sergey Kazberovich <kazbers@gmail.com>
2024-05-09 23:39:43 +0400

    injected app bar, header and tab bar into the q/a view

--- lib/src/quick_access/presentation/views/quick_access_view.dart
+++ lib/src/quick_access/presentation/views/quick_access_view.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:tdd_education_app/core/common/widgets/gradient_background.dart';
+import 'package:tdd_education_app/core/res/media_resources.dart';
+import 'package:tdd_education_app/src/quick_access/presentation/refactors/quick_access_app_bar.dart';
+import 'package:tdd_education_app/src/quick_access/presentation/refactors/quick_access_header.dart';
+import 'package:tdd_education_app/src/quick_access/presentation/refactors/quick_access_tab_bar.dart';
 
 class QuickAccessView extends StatelessWidget {
   const QuickAccessView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
+      appBar: QuickAccessAppBar(),
+      body: GradientBackground(
+        image: MediaRes.documentsGradientBackground,
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: QuickAccessHeader(),
+              ),
+              Expanded(
+                child: QuickAccessTabBar(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
