05fd94e84e91c16e2a1eb6f4de4e6f47ba424d60
Sergey Kazberovich <kazbers@gmail.com>
2024-05-13 23:52:05 +0400

    added history details screen with answer tile

--- lib/src/quick_access/presentation/views/exam_history_details_screen.dart
+++ lib/src/quick_access/presentation/views/exam_history_details_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:tdd_education_app/src/course/features/exams/domain/entities/user_exam.dart';
+import 'package:tdd_education_app/src/quick_access/presentation/widgets/exam_history_answer_tile.dart';
+import 'package:tdd_education_app/src/quick_access/presentation/widgets/exam_history_tile.dart';
 
 class ExamHistoryDetailScreen extends StatelessWidget {
   const ExamHistoryDetailScreen({
@@ -13,6 +16,53 @@ class ExamHistoryDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: Text('${exam.examTitle} Details')),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ExamHistoryTile(exam: exam, shouldNavigateToDetails: false),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: 'Date Submitted: ',
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: DateFormat.yMMMMd().format(exam.dateSubmitted),
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.separated(
+                  itemCount: exam.answers.length,
+                  separatorBuilder: (_, __) => const Divider(
+                    thickness: 1,
+                    color: Color(0xFFE6E8EC),
+                  ),
+                  itemBuilder: (_, index) {
+                    final answer = exam.answers[index];
+                    return ExamHistoryAnswerTile(answer, index: index);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
