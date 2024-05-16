1500ced6f2357c7eaf2e1145fcdeb23aea041e9c
Sergey Kazberovich <kazbers@gmail.com>
2024-05-16 21:40:33 +0400

    tested get groups usecase

---
+++ test/src/chat/domain/usecases/chat_repo.mock.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:tdd_education_app/src/chat/domain/repositories/chat_repository.dart';
+
+class MockChatRepo extends Mock implements ChatRepo {}
