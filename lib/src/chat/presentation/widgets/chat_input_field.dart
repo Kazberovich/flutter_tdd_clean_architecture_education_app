4d45fd6c1c0fe17fc52d34172cf977058f69b324
Sergey Kazberovich <kazbers@gmail.com>
2024-09-18 11:43:17 +0400

    added chat input field

---
+++ lib/src/chat/presentation/widgets/chat_input_field.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatImputField extends StatefulWidget {
+  const ChatImputField({super.key});
+
+  @override
+  State<ChatImputField> createState() => _ChatImputFieldState();
+}
+
+class _ChatImputFieldState extends State<ChatImputField> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
